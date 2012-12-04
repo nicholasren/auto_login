5720ac0fd1c1932fb469769c29089b878b044b82
Ren Xiaojun <xjren@thoughtworks.com>
2012-12-04 22:40:39 +0800

    Init commit

---
+++ github.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'watir-webdriver'
+b = Watir::Browser.new
+b.goto 'github.com/login'
+b.text_field(:id => 'login_field').set 'nicholas85211@gmail.com'
+b.text_field(:id => 'password').set 'your password'
+b.button(:name => 'commit').click
